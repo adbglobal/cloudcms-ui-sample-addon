4d2c6ab53a0aa15029d590554100aa9842f82575
Marco Storto <m.storto@adbglobal.com>
2017-05-09 15:54:08 +0200

    fix subsription, add options mangle for slave

--- sample/fields/command-field.js
+++ sample/fields/command-field.js
@@ -64,10 +64,27 @@ define(function(require, exports, module) {
                     return newSchema
                 } else {
                     var newSchema = {}
-                    if (!schema.isVariant) {
-                        newSchema["readonly"] = true
-                    }
                     Object.assign(newSchema, schema)
+                    if (!schema.isVariant) {
+                        newSchema["readonly"] = true;
+                        newSchema["format"] = "text";
+                    }
+                    return newSchema
+                }
+            }
+
+            function makeSlaveOptions(schema) {
+                if (!Alpaca.isUndefined(schema.fields)) {
+                    var newSchema = {};
+                    Object.assign(newSchema, schema)
+                    newSchema.fields = _.mapValues(schema.fields, makeSlaveOptions);
+                    return newSchema
+                } else {
+                    var newSchema = {}
+                    Object.assign(newSchema, schema)
+                    if (schema.type == "select") {
+                        newSchema.type = "text"
+                    }
                     return newSchema
                 }
             }
@@ -77,9 +94,11 @@ define(function(require, exports, module) {
             self.connector.branch.queryOne({ "_doc": nodeId }).then(function() {
                 if (self.options.isSlave) {
                     var masterSchema = {};
+                    var masterOptions = {};
                     loadCacheAttachment(masterSchema, this, 'schema');
                     Object.assign(self.schema, makeSlaveSchema(masterSchema))
-                    loadCacheAttachment(self.options, this, 'options');
+                    loadCacheAttachment(masterOptions, this, 'options');
+                    Object.assign(self.options, makeSlaveOptions(masterOptions))
                 } else {
                     loadCacheAttachment(self.schema, this, 'schema');
                     loadCacheAttachment(self.options, this, 'options');
@@ -100,12 +119,15 @@ define(function(require, exports, module) {
                     var dep = self.top().getControlByPath(self.options.dependentField);
                     //console.log(self.name, dep)
                     if (dep) {
-                        self.subscribe(dep, function(value) {
-                            if (value)
-                                self.updateSchemaOptions(value.id, function() {
-                                    self.refresh();
-                                })
-                        });
+                        if (!self.subscribed) {
+                            self.subscribed = true;
+                            self.subscribe(dep, function(value) {
+                                if (value)
+                                    self.updateSchemaOptions(value.id, function() {
+                                        self.refresh();
+                                    })
+                            });
+                        }
                         if (dep.data) {
                             self.updateSchemaOptions(dep.data.id, function() {
                                 self.refresh();
@@ -116,6 +138,15 @@ define(function(require, exports, module) {
                 var dep = self.top().getControlByPath(self.options.dependentField);
                 if (dep && dep.data)
                     this.base(function() {
+                        if (!self.subscribed) {
+                            self.subscribed = true;
+                            self.subscribe(dep, function(value) {
+                                if (value)
+                                    self.updateSchemaOptions(value.id, function() {
+                                        self.refresh();
+                                    })
+                            });
+                        }
                         self.updateSchemaOptions(dep.data.id, callback)
                     })
                 else {
